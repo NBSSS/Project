5bc04a9e874a820fdaf6bba977514442f46ff3c9
Aashish Atrey <aatrey882@gmail.com>
2017-05-25 10:45:04 +0530

    Cart

---
+++ backendProject/src/main/java/com/niit/backendProject/model/Item.java
@@ -0,0 +1,11 @@
+package com.niit.backendProject.model;
+
+import javax.persistence.Entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+public class Item {
+
+}
---
+++ backendProject/src/main/java/com/niit/backendProject/model/Payment.java
@@ -0,0 +1,5 @@
+package com.niit.backendProject.model;
+
+public class Payment {
+
+}
